175e8e12cfda510d752eca85b92c2e7249230447
Derek Tanaka <tanaka.derek@gmail.com>
2019-07-28 14:36:44 -0400

    adding routing, homepage

---
+++ src/app/home/home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { usersListOfTickers } from '../common/testData'
+import WatchList from '../watchlist/watchlist'
+import Login from '../authentication/login'
+
+function App() {
+    return (
+        <div className='App'>
+            <Login />
+            <WatchList listOfTicker={usersListOfTickers} />
+        </div>
+    )
+}   
+
+export default App
